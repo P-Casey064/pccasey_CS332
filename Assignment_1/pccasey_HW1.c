dfd9e96e280e03cca7517fde9dba4c84d3dffdad
P-Casey064 <pcasey064@att.net>
2026-06-02 20:58:24 +0000

    fix_this

--- Assignment_1/pccasey_HW1.c
+++ Assignment_1/pccasey_HW1.c
@@ -5,7 +5,25 @@ CS332
 
 #include <stdio.h>
 
+int sumOfDigits()
+{
+    char n[3];
+    printf("Enter a number: ");
+    scanf("%s", n);
+    int total = 0;
+    int length = (sizeof(n) / sizeof(n[0]));
+    for(int i = 0; i < length; i++)
+    {
+        printf("%c\n", n[i]);
+        total = total + ("%d", n[i]);
+    }
+    printf("%d\n", total);
+    return 0;
+}
+
+
 int main(int argc, char** argv)
 {
-    
+    sumOfDigits();
 }
+
