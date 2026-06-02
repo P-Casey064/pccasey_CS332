2d0e877b8fe598b9d7f6fd99b03c6f967c1e714b
P-Casey064 <pcasey064@att.net>
2026-06-02 18:21:25 +0000

    first_lab_assignment

---
+++ lab1/labAssignment1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/*to compile in terminal
+gcc -o labAssignment1 labAssignment.c
+
+to run in terminal
+./labAssignment1
+
+*/
+int main(int argc, char** argv)
+{
+    int none = 0;
+    int prime = 0;
+    int given_number;
+    printf("Enter a number: ");
+    scanf("%d", &given_number);
+
+    if(given_number == 2)
+    {
+        prime = 1;
+        printf("Is a Prime\n");
+        return 0;
+    }
+    for (int i = 2; i <= 10; i++)
+    {
+        if(given_number % i == 0)
+        {
+            none++;
+        }
+        else{
+            prime++;
+        }
+    }
+    if(none > 0)
+    {
+        printf("Is not a Prime\n");
+        return 0;
+    }
+    else{
+        printf("Is a Prime\n");
+        return 0;
+    }
+
+    
+}
