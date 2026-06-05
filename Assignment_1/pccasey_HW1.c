288574ea7dac6ca1d40f2271b5b828f22eae1a26
P-Casey064 <pcasey064@att.net>
2026-06-05 17:57:59 +0000

    Q1_Done

--- Assignment_1/pccasey_HW1.c
+++ Assignment_1/pccasey_HW1.c
@@ -1,21 +1,24 @@
-/*
-pccasey
-CS332
-*/
-
 #include <stdio.h>
 
-int sumOfDigits()
+int sumOfDigits(int A)
 {
-    char n[3];
+    char n[A];
     printf("Enter a number: ");
     scanf("%s", n);
     int total = 0;
     int length = (sizeof(n) / sizeof(n[0]));
-    for(int i = 0; i < length; i++)
+    if(n[0] == '-' || (("%d", n[0]) - '0') == 0)
     {
-        printf("%c\n", n[i]);
-        total = total + ("%d", n[i]);
+        printf("-1\n");
+        return 0;
+    }
+
+    for(int i = 0; i < A; i++)
+    {
+        if(n[i] != '\0')
+        {
+            total = total + (("%d", n[i]) - '0');
+        }
     }
     printf("%d\n", total);
     return 0;
@@ -24,6 +27,8 @@ int sumOfDigits()
 
 int main(int argc, char** argv)
 {
-    sumOfDigits();
+    int A;
+    printf("Input the length of the number: ");
+    scanf("%d", &A);
+    sumOfDigits(A);
 }
-
