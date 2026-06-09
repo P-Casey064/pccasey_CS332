6a24871466fd000cd814aab6c7e0aa47dfe3a5b8
P-Casey064 <pcasey064@att.net>
2026-06-09 18:45:43 +0000

    even_done

--- Assignment_1/pccasey_HW1.c
+++ Assignment_1/pccasey_HW1.c
@@ -16,11 +16,11 @@ int sumOfDigits(char n[], int A)
             total = total + (("%d", n[i]) - '0');
         }
     }
-    printf("total: %d\n", total);
+    printf("total: %d\n\n", total);
     return 0;
 }
 
-int UABMaxMinDiff(int A, int n[])
+int UABMaxMinDiff(int n[], int A)
 {
     int min, max;
     min = n[0];
@@ -36,10 +36,30 @@ int UABMaxMinDiff(int A, int n[])
             min = n[i];
         }
     }
-    printf("%d\n", max);
-    printf("%d\n", min);
     int diff = max - min;
-    printf("Difference: %d\n", diff);
+    printf("Difference: %d\n\n", diff);
+    return 0;
+}
+
+int replaceEvenWithZero(int n[], int A)
+{
+    for(int i = 0; i < A; i++)
+    {
+        if(n[i] % 2 == 0)
+        {
+            n[i] = 0;
+        }
+    }
+    printf("[");
+    for(int i = 0; i < A; i++)
+    {
+        printf("%d", n[i]);
+        if(i < (A - 1))
+        {
+            printf(", ");
+        }
+    }
+    printf("]\n\n");
     return 0;
 }
 
@@ -65,5 +85,15 @@ int main(int argc, char** argv)
         printf("Input an element for the array: ");
         scanf("%d", &t[i]);
     }
-    UABMaxMinDiff(A, t);
+    UABMaxMinDiff(t, A);
+
+/*replaceEvenWithZero*/
+    printf("Input the length of the Array: ");
+    scanf("%d", &A);
+    for(int i = 0; i < A; i++)
+    {
+        printf("Input an element for the array, even or odd: ");
+        scanf("%d", &t[i]);
+    }
+    replaceEvenWithZero(t, A);
 }
