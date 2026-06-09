b9cd7b08061336d85a985e076a36c13534596d5a
P-Casey064 <pcasey064@att.net>
2026-06-09 16:25:26 +0000

    laab2_done

---
+++ lab02/lab2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int insertionSort(int n[], int A)
+{
+    int temp, loc;
+    for(int i = 0; i < A; i++)
+    {
+        loc = i;
+        while (loc > 0 && n[loc - 1] > n[loc])
+        {
+            temp = n[loc];
+            n[loc] = n[loc - 1];
+            n[loc-1] = temp;
+            loc--;
+        }
+    }
+    printf("[");
+    for(int i = 0; i < A; i++)
+    {
+        printf("%d,", n[i]);
+    }
+    printf("]\n");
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+    int A;
+    int z;
+    printf("Input the length of the array: ");
+    scanf("%d", &A);
+    int n[A];
+
+    for(int i = 0; i < A; i++)
+    {
+        printf("Please enter an element of the array: ");
+        scanf("%d", &n[i]);
+    }
+
+    insertionSort(n, A);
+
+}
