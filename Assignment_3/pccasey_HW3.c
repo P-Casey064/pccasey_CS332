1cf1d488437c042c31d2d4ddaf2a0a909e1a274f
P-Casey064 <pcasey064@att.net>
2026-07-31 18:15:23 +0000

    working

--- Assignment_3/pccasey_HW3.c
+++ Assignment_3/pccasey_HW3.c
@@ -6,9 +6,15 @@
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        printf("No arguments given, exiting program");
+        return 0;
+    }
     const char *start_dir = argv[1];
     DIR *dir = opendir(start_dir);
 
@@ -16,7 +22,7 @@ int main(int argc, char *argv[])
     {
         if (errno == ENOENT) 
         {
-            printf("Error: The directory '%s' does not exist.\n", dir_path);
+            printf("Error: The directory '%s' does not exist.\n", start_dir);
         } else 
         {
             printf("Error opening directory: %s\n", strerror(errno));
@@ -24,9 +30,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL)
     {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
         pid_t pid = fork();
 
         if (pid < 0)
@@ -39,7 +50,18 @@ int main(int argc, char *argv[])
         {
             //this will contain the meat of the program. Print file name, file size in bytes, 
             //how many words the file contains for .txt files
+            char fullPath[4096]; 
+            snprintf(fullPath, sizeof(fullPath), "%s/%s", start_dir, entry->d_name);
+            printf("%s\n", entry->d_name);
+            exit(0);
+        }
+        else //parent process
+        {
+            int status;
+            waitpid(pid, &status, 0);
         }
 
     }
+    closedir(dir);
+    return 0;
 }
