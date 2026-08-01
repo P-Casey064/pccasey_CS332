d49417da04ecfa89f11cde2f6f23d2e2484de6fd
P-Casey064 <pcasey064@att.net>
2026-08-01 01:11:34 +0000

    done

---
+++ Assignment_3/README.txt
@@ -0,0 +1,9 @@
+to compile: $ gcc -Wall pccasey_HW3.c -o HW3
+or          $ make
+
+to run:     $ ./HW3 <directory>
+
+tested with different file types. the .txt files are the only ones recieving a word count.
+Largest file size was 16000 and largest word count tested was 624 (mathmatically counted correctly)
+
+does not go into subdirectories and only sees the files in the given directory
--- Assignment_3/pccasey_HW3.c
+++ Assignment_3/pccasey_HW3.c
@@ -19,9 +19,44 @@ to run:     $ ./HW3 <directory>
 #include <errno.h>
 #include <sys/wait.h>
 
-int read_txt()
+
+int read_txt(const char *name)
 {
-    
+    int count = 0;
+    int is_char = 0;
+    int space;
+    FILE *file = fopen(name, "r");
+    if (file == NULL) //this checks if the file opened correctly or not
+    {
+        if (errno == EACCES) //checks read permissions
+        {
+            fprintf(stderr, "Error: You do not have permission to read this file.\n");
+        } 
+        else if (errno == ENOENT) //if the file does not exist
+        {
+            fprintf(stderr, "Error: The file does not exist.\n");
+        } 
+        else //if it failed to open for some other reason
+        {
+            fprintf(stderr, "File open failed: %s\n", strerror(errno));
+        }
+        return -1;
+    }
+    while ((space = fgetc(file)) != EOF) //this iterates through each character till end of file
+    {
+        if(space == ' ' || space == '\t' || space == '\n') //checks for each type of space
+        { //this reads it as a whitespace
+            is_char = 0;
+        }
+        else if (!is_char) //this reads as a new word and adds a count
+        {
+            is_char = 1;
+            count++;
+        }
+    }
+
+    fclose(file);
+    return count;
 }
 
 
@@ -30,7 +65,7 @@ int main(int argc, char *argv[])
 {
     if (argc < 2) //checks for directory argument
     {
-        printf("Usage: ./HW3 <directory_name>");
+        printf("Usage: ./HW3 <directory_name>\n");
         return 0;
     }
     const char *start_dir = argv[1];
@@ -83,11 +118,12 @@ int main(int argc, char *argv[])
             if(strstr(entry->d_name, ".txt") != NULL) //checks for .txt files
             {
                 //call to a seperate method for word count
-                printf("File: %s | Size: %ld | Words: \n", entry->d_name, (long)st.st_size);
+                int words = read_txt(fullPath);
+                printf("File: %s | Size: %ld | Words: %d\n", entry->d_name, (long)st.st_size, words); //adds word count for .txt files
             }
             else
             {
-                printf("File: %s | Size: %ld\n", entry->d_name, (long)st.st_size);
+                printf("File: %s | Size: %ld\n", entry->d_name, (long)st.st_size); //no word count for non .txt files
             }
             exit(0);
         }
